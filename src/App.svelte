b613d6e4bc7c4e656c3549327b9790975c7ac184
Extrogenesis <extrogenesis@gmail.com>
2019-11-06 13:12:15 -0500

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,73 @@
+<script>
+	import Header from "./components/Header.svelte"
+
+</script>
+
+<style>
+
+	.wrapper {
+		background-image: url('img/bg-min.jpg');
+		height: 95vh;
+		min-height: 950px;
+		max-height: 1936px;
+		background-position: 50% 30%;
+		text-align: center;
+		
+	}
+	.bumper {
+		height: 20px;
+		background-color: #383737;
+	}
+
+	.caption {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, 0%);
+		width: 86%;
+		color: #F5F3F1;
+	}
+
+	.caption h1 {
+		font-size: 60px;
+    	padding: 0 10%;
+    	font-weight: 900;
+	}
+
+	.date-location {
+		margin-top: 80px;
+	}
+
+	.date-location p {
+		font-weight: 300;
+    	font-size: 30px;
+	}
+	
+	.book-now {
+		display: flex;
+		height: 80px;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
+
+
+
+<section class="wrapper">
+	<Header />
+	<div class="bumper"></div>
+	<div class="caption">
+		<h1>Frank's Bowling's First Major Retrospective</h1>
+		<div class="date-location">
+			<p><strong>Tate Britan, London</strong></p>
+			<p style="margin: 30px">31 May - 26 August 2019</p>
+		</div>
+	</div>
+</section>
+
+<main>
+	<section class="book-now"><h2>BOOK NOW</h2></section>
+	<section class="gallery"></section>
+</main>
+
+
---
+++ src/components/Header.svelte
@@ -0,0 +1,48 @@
+<script></script>
+
+
+<style>
+
+    header {
+        display: flex;
+        justify-content: center;
+        background-color: #F5F3F1
+    }
+
+    nav {
+        width: 86%;
+        height: 99px;
+        display: grid;
+        grid: repeat(2, 1fr) / repeat(5, 1fr);
+        border-left: 2px solid #383737;
+        border-right: 2px solid #383737;
+    }
+    nav div{
+        display: flex;
+        align-items: center;
+        padding-left: 12px;
+    }
+    .title {
+        grid-row: 1 / 3;
+        grid-column: 1 / 4;
+    }
+    
+    .nav-item{
+        border-left: 2px solid #383737;
+    }
+
+</style>
+
+<header>
+    <nav>
+        <div class="title">Frank <span>Bowling</span></div>
+        <div class="nav-item" style="border-bottom: 2px solid #383737;">
+            <a href="#">Exhibitions</a>
+        </div>
+        <div class="nav-item" style="border-bottom: 2px solid #383737;">
+            <a href="#">Work</a>
+        </div>
+        <div class="nav-item"><a href="#">About</a></div>
+        <div class="nav-item"><a href="#">Contract</a></div>
+    </nav>
+</header>
